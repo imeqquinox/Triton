cbd016f55cbc0f458080ed7a121eb9a8b3838430
Lewis Wilson <lewiswilson1998@hotmail.co.uk>
2022-10-12 20:12:50 +0100

    Temp jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float jumpForce = 3f;
+    [SerializeField] private LayerMask groundLayer; 
 
     private Rigidbody2D body;
     private float horizontalMove = 0f;
+    private bool isGrounded = false;
+    private Transform groundCheck;
 
     private void Awake()
     {
-        body = GetComponent<Rigidbody2D>(); 
+        body = GetComponent<Rigidbody2D>();
+        groundCheck = GetComponentInParent<Transform>().Find("GroundCheck"); 
     }
 
     private void Update()
@@ -22,6 +27,13 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         var velocityX = moveSpeed * horizontalMove;
-        body.velocity = new Vector2(velocityX, body.velocity.y); 
+        body.velocity = new Vector2(velocityX, body.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f, groundLayer);
+
+        if (isGrounded && Input.GetKey(KeyCode.W))
+        {
+            body.velocity = new Vector2(body.position.x, jumpForce); 
+        }
     }
 }
